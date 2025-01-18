289d0c9e915a3addb0228594b26f3ccca2119274
Jirawat <jirawatsuanmok@gmail.com>
2025-01-18 07:26:18 +0700

    [feat] add script preline

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,11 @@ export default defineNuxtConfig({
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
+      script: [
+        {
+          src: "/node_modules/preline/dist/preline.js",
+        },
+      ],
     },
   },
 
