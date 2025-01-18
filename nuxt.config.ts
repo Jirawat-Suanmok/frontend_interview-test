6953842065aa7f1dd9571a35d7f2a7463e7337f1
Jirawat <jirawatsuanmok@gmail.com>
2025-01-18 17:27:39 +0700

    [fix] nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,11 +7,6 @@ export default defineNuxtConfig({
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
-      script: [
-        {
-          src: "/node_modules/preline/dist/preline.js",
-        },
-      ],
     },
   },
 
